c601fd67d37e29a1308b6e6ad69776d0d1461066
Brian Apeli <apelibrian@gmail.com>
2022-01-31 19:01:41 +0300

    introduction

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -2,9 +2,27 @@ import './intro.scss';
 import { useEffect, useRef } from 'react';
 
 export default function Intro() {
-   
+  const textRef = useRef();
+
   return (
-   <div className="intro" id="intro">
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Brian Apeli</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import "./portfolio.scss"
 
 export default function Portfolio() {
-  return <div className="portfolio" id="potfolio">
+  return <div className="portfolio" id="portfolio">
   </div>;
 }
