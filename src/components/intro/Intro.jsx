0f134e7cbb079bae4afcda968e1ccb50cc5212a7
Brian Apeli <apelibrian@gmail.com>
2022-01-31 19:29:42 +0300

    react-text-typing-animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,18 @@
 import './intro.scss';
+import {init} from "ityped";
 import { useEffect, useRef } from 'react';
 
 export default function Intro() {
   const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed:60,
+      strings: ["Web Developer", "Web Designer", "Content Creator"],
+    });
+  }, []);
 
   return (
     <div className="intro" id="intro">
